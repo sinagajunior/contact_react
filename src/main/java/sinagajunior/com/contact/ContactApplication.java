addb82729b649c78e01da74149218d6892d2f81b
Roy <sinagajunior@gmail.com>
2018-11-25 12:01:21 +0700

    add comment where this tutorial come from

--- src/main/java/sinagajunior/com/contact/ContactApplication.java
+++ src/main/java/sinagajunior/com/contact/ContactApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactApplication {
+	/**
+	 * Adapt from example
+	 * https://www.djamware.com/post/5ab6397c80aca714d19d5b9c/building-spring-boot-mongodb-and-reactjs-crud-web-application
+	 * @param args
+	 */
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactApplication.class, args);
